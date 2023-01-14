b1926d7fcf6d24f7a4c478fbede7ec06cde75dc0
bartek <smalecbartlomiej@gmail.com>
2023-01-14 09:19:32 +0100

    Changed rest client to by fully reactive with thymeleaf

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,9 +2,13 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -27,5 +31,16 @@ public class ApiServiceImpl implements ApiService {
                 .queryParam("_limit", limit);
         List<User> userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), List.class);;
         return userData;
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("_limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(User.class);
     }
 }
